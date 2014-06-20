28ab430c0aa4cff1575a8e9016eecc2057e0ed03
Sean Robertson <sprobertson@gmail.com>
2014-06-20 16:28:39 -0700

    lib/env -> bin/env

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ coffee -o lib -c src
 
 # Ugly hack to prepend shebang line
 cp lib/registry.js lib/registry.js.tmp
-echo "#!/usr/lib/env node" > lib/registry.js.tmp
+echo "#!/usr/bin/env node" > lib/registry.js.tmp
 cat lib/registry.js >> lib/registry.js.tmp
 mv lib/registry.js.tmp lib/registry.js
 
